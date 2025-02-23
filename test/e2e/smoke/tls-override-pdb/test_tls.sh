9197985274c196233cac568b475632a814cc46b5
whg517 <huagang517@126.com>
2025-02-23 13:26:31 +0800

    build(deps): bump tools version (#174)
    * build(deps): bump tools version
    
    * build(deps): bump golint version
    
    * build(deps): bump chainsaw version
    
    * test: add debug msg to e2e
    
    * fix(e2e): fix test_tls.sh file syntax error
    
    * fix(e2e): fix test tls check error

--- test/e2e/smoke/tls-override-pdb/test_tls.sh
+++ test/e2e/smoke/tls-override-pdb/test_tls.sh
@@ -20,6 +20,7 @@ max_retries=2
 retry_delay=5
 
 # Try connection with retries
+echo "Testing unsecure connection..."
 while [ $retry_count -le $max_retries ]; do
     if /kubedoop/zookeeper/bin/zkCli.sh -server "${SERVER}" ls / &> /dev/null; then
         echo "[SUCCESS] Unsecure client connection established!"
@@ -39,6 +40,7 @@ done
 ############################################################################
 # We set the correct client tls credentials and expect to be able to connect
 ############################################################################
+echo "Testing secure connection with client certificates..."
 CLIENT_STORE_SECRET="$(< /kubedoop/config/zoo.cfg grep "ssl.keyStore.password" | cut -d "=" -f2)"
 export CLIENT_STORE_SECRET
 export CLIENT_JVMFLAGS="
@@ -50,9 +52,13 @@ export CLIENT_JVMFLAGS="
 -Dzookeeper.ssl.trustStore.location=/kubedoop/server_tls/truststore.p12
 -Dzookeeper.ssl.trustStore.password=${CLIENT_STORE_SECRET}"
 
-if ! /kubedoop/zookeeper/bin/zkCli.sh -server "${SERVER}" ls / &> /dev/null;
-then
+output=$(/kubedoop/zookeeper/bin/zkCli.sh -server "${SERVER}" ls / 2>&1)
+if [ $? -ne 0 ]; then
   echo "[ERROR] Could not establish secure connection using client certificates!"
+  echo "Command output:"
+  echo "----------------------------------------"
+  echo "$output"
+  echo "----------------------------------------"
   exit 1
 fi
 echo "[SUCCESS] Secure and authenticated client connection established!"
@@ -60,6 +66,7 @@ echo "[SUCCESS] Secure and authenticated client connection established!"
 ############################################################################
 # We set the (wrong) quorum tls credentials and expect to fail (wrong certificate)
 ############################################################################
+echo "Testing secure connection with quorum certificates..."
 QUORUM_STORE_SECRET="$(< /kubedoop/config/zoo.cfg grep "ssl.quorum.keyStore.password" | cut -d "=" -f2)"
 export QUORUM_STORE_SECRET
 export CLIENT_JVMFLAGS="
@@ -71,12 +78,17 @@ export CLIENT_JVMFLAGS="
 -Dzookeeper.ssl.trustStore.location=/kubedoop/quorum_tls/truststore.p12
 -Dzookeeper.ssl.trustStore.password=${QUORUM_STORE_SECRET}"
 
-if /kubedoop/zookeeper/bin/zkCli.sh -server "${SERVER}" ls / &> /dev/null;
-then
+output=$(/kubedoop/zookeeper/bin/zkCli.sh -server "${SERVER}" ls / 2>&1)
+if [ $? -eq 0 ]; then
   echo "[ERROR] Could establish secure connection with quorum certificates (should not be happening)!"
+  echo "Command output:"
+  echo "----------------------------------------"
+  echo "$output"
+  echo "----------------------------------------"
   exit 1
 fi
 echo "[SUCCESS] Could not establish secure connection with (wrong) quorum certificates!"
+
 
 echo "All TLS tests successful!"
 exit 0
